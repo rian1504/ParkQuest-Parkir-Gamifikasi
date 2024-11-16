f76eb73c47360940e4b13d7dbfd410020e88ec31
juanngln <juanjonathan05@gmail.com>
2024-11-16 15:13:37 +0700

    update security

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:parkquest_parkir_gamifikasi/pages/cari_parkir.dart';
 import 'package:parkquest_parkir_gamifikasi/pages/form_rekomendasi.dart';
 import 'package:parkquest_parkir_gamifikasi/pages/halaman_awal.dart';
 import 'package:parkquest_parkir_gamifikasi/pages/daftar.dart';
@@ -7,7 +9,8 @@ import 'package:parkquest_parkir_gamifikasi/pages/dashboard.dart';
 import 'package:parkquest_parkir_gamifikasi/pages/rekomendasi_parkir.dart';
 import 'package:parkquest_parkir_gamifikasi/pages/survey.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -25,7 +28,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home: HalamanAwal(),
+      home: RekomendasiParkir(),
       routes: {
         '/login': (context) => Login(),
         '/daftar': (context) => Daftar(),
@@ -33,6 +36,7 @@ class MyApp extends StatelessWidget {
         '/survey': (context) => Survey(),
         '/rekomendasi_parkir': (context) => RekomendasiParkir(),
         '/form_rekomendasi': (context) => FormRekomendasi(),
+        '/cari_parkir': (context) => CariParkir(),
       },
     );
   }
