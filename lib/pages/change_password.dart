a89632feed38d04a7001f0c759c7a2f502924706
juanngln <juanjonathan05@gmail.com>
2024-12-17 16:08:15 +0700

    Update halaman profile 1. Update halaman profile 2. Membuat halaman edit profile dan change password 3. Update halaman inventory

---
+++ lib/pages/change_password.dart
@@ -0,0 +1,372 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:parkquest_parkir_gamifikasi/pages/profile.dart';
+import 'package:show_hide_password/show_hide_password.dart';
+
+class ChangePassword extends StatefulWidget {
+  const ChangePassword({super.key});
+
+  @override
+  State<ChangePassword> createState() => _ChangePasswordState();
+}
+
+class _ChangePasswordState extends State<ChangePassword> {
+  // Password Field
+  Widget _buildPasswordField(String label) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      child: SizedBox(
+        height: 50,
+        width: double.infinity,
+        child: ShowHidePassword(
+          hidePassword: true,
+          passwordField: (hidePassword) {
+            return TextField(
+              obscureText: hidePassword,
+              decoration: InputDecoration(
+                labelText: label,
+                labelStyle: GoogleFonts.inter(
+                  color: Colors.black,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w300,
+                ),
+                contentPadding: EdgeInsets.symmetric(horizontal: 20),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            );
+          },
+          iconSize: 20,
+          visibleOffIcon: CupertinoIcons.eye_slash,
+          visibleOnIcon: CupertinoIcons.eye,
+        ),
+      ),
+    );
+  }
+
+  // Confirmation Dialog
+  Future<void> _showConfirmationDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          contentPadding: EdgeInsets.all(20),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Stack(
+                children: [
+                  Column(
+                    children: [
+                      Image.asset(
+                        'assets/img/question_mark.png',
+                        width: 200,
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'Apakah anda yakin?',
+                        style: GoogleFonts.inter(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            // Button Ya
+            Center(
+              child: SizedBox(
+                width: 130,
+                height: 40,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    _showSuccessDialog();
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xFFFEC827),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Ya',
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            // Button Tidak
+            Center(
+              child: SizedBox(
+                width: 130,
+                height: 40,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xFFFEC827),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Tidak',
+                    style: GoogleFonts.inter(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  // Success Dialog
+  Future<void> _showSuccessDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          contentPadding: EdgeInsets.all(20),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Stack(
+                children: [
+                  Column(
+                    children: [
+                      Image.asset(
+                        'assets/img/success_checked.png',
+                        width: 100,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Kata Sandi Berhasil Diperbarui!',
+                        style: GoogleFonts.inter(
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Profile(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF24E1E),
+                        ),
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFEC827),
+        title: Text(
+          'Ubah Kata Sandi',
+          style: GoogleFonts.inter(
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(
+            CupertinoIcons.chevron_left,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          // Top Right Circle
+          Align(
+            alignment: Alignment.topRight,
+            child: CustomPaint(
+              size: Size(150, 150),
+              painter: CirclePainterTop(),
+            ),
+          ),
+          // Bottom Left Circle
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: CustomPaint(
+              size: Size(150, 150),
+              painter: CirclePainterBottom(),
+            ),
+          ),
+          // Card
+          Center(
+            child: SingleChildScrollView(
+              child: Container(
+                width: double.infinity,
+                height: 500,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 20,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          // Profile Picture
+                          CircleAvatar(
+                            radius: 40,
+                            child: Icon(Icons.person),
+                          ),
+                          SizedBox(
+                            height: 40,
+                            child: Center(
+                              child: Text(
+                                'Akun Saya',
+                                style: GoogleFonts.inter(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Divider(),
+                          // Password Form Field
+                          _buildPasswordField('Password Lama'),
+                          _buildPasswordField('Password Baru'),
+                          _buildPasswordField('Konfirmasi Password'),
+                          SizedBox(height: 20),
+                          // Save Button
+                          SizedBox(
+                            width: 125,
+                            child: TextButton(
+                              onPressed: () {
+                                _showConfirmationDialog();
+                              },
+                              style: TextButton.styleFrom(
+                                backgroundColor: Color(0xFFFECE2E),
+                              ),
+                              child: Text(
+                                'Simpan',
+                                style: GoogleFonts.inter(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Top Right Circle
+class CirclePainterTop extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()..color = Color(0xFFFFB33B);
+
+    Offset center = Offset(size.width / 1.5, size.height / 3);
+
+    canvas.drawCircle(center, 100, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+// Bottom Left Circle
+class CirclePainterBottom extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()..color = Color(0xFFFFB33B);
+
+    Offset center = Offset(size.width / 3, size.height / 1.5);
+
+    canvas.drawCircle(center, 100, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
--- lib/pages/inventory.dart
+++ lib/pages/inventory.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Inventory extends StatefulWidget {
   const Inventory({super.key});
@@ -8,14 +10,15 @@ class Inventory extends StatefulWidget {
   State<Inventory> createState() => _InventoryState();
 }
 
-// Default avatar
 class _InventoryState extends State<Inventory> {
-  String _selectedButton = "basic";
+  final String _selectedButton = "basic";
   String _selectedAvatar = 'assets/img/basic_avatar.png';
   String _selectedAvatarName = "Old Car";
+  var selectedCategory = "Basic";
 
-// List Categories
+  // Categories
   final List<Category> categories = [
+    // Basic Category
     Category("basic", const Color(0xFFD9D9D9), "Basic", [
       "assets/img/basic_avatar.png",
       "assets/img/basic_bike_avatar.png",
@@ -23,6 +26,7 @@ class _InventoryState extends State<Inventory> {
       "Old Car",
       "Scooter",
     ]),
+    // Rare Category
     Category("rare", const Color(0xFF2592E7), "Rare", [
       "assets/img/rare_avatar.png",
       "assets/img/rare_bike_avatar.png",
@@ -30,6 +34,7 @@ class _InventoryState extends State<Inventory> {
       "Blue Sedan",
       "Army Bike",
     ]),
+    // Legendary Category
     Category("legendary", const Color(0xFFF71010), "Legendary", [
       "assets/img/legendary_avatar.png",
       "assets/img/legendary2_avatar.png",
@@ -41,202 +46,190 @@ class _InventoryState extends State<Inventory> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          backgroundColor: Colors.amber[300],
-          title: const Text(
-            "Inventory",
-            style: TextStyle(color: Colors.white),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFEC827),
+        title: Text(
+          'Inventory',
+          style: GoogleFonts.inter(
+            fontWeight: FontWeight.w500,
+            color: Colors.white,
           ),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(height: 35),
-              _buildAvatarSection(),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 50,
-                    vertical: 5,
-                  ),
-                  backgroundColor: Colors.amber[300],
-                ),
-                child: const Text(
-                  "Gunakan",
-                  style: TextStyle(fontSize: 18, color: Colors.white),
-                ),
-              ),
-              const SizedBox(height: 40),
-              _buildCategoryButtons(),
-              const SizedBox(height: 16),
-              // Divider 1
-              const Divider(
-                thickness: 1,
-                color: Colors.black,
-                height: 25,
-              ),
-              _buildSelectedCategoryLabel(),
-              // Divider 2
-              const Divider(
-                thickness: 1,
-                color: Colors.black,
-                height: 0,
-              ),
-              Container(
-                color: const Color(0xFFFFD05E),
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.5,
-                  child: _buildItemGrid(),
-                ),
-              ),
-            ],
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: Icon(
+            CupertinoIcons.chevron_left,
+            color: Colors.white,
           ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Avatar
+            _buildAvatarSection(),
+            // Category
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: 24,
+                horizontal: 16,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildCategoryButton(
+                    'Basic',
+                    "Basic",
+                    Color(0xFFD9D9D9),
+                    Color(0xFF545454),
+                  ),
+                  _buildCategoryButton(
+                    'Rare',
+                    "Rare",
+                    Color(0xFF5EB8FF),
+                    Color(0xFF003966),
+                  ),
+                  _buildCategoryButton(
+                    'Legendary',
+                    "Legendary",
+                    Color(0xFFFF2323),
+                    Color(0xFF5D0000),
+                  ),
+                ],
+              ),
+            ),
+            // My Avatar
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: 12,
+                  left: 16,
+                  right: 16,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 28),
+                      child: Text(
+                        'My Avatar',
+                        style: GoogleFonts.inter(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: _buildItemGrid(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  // Button Categories
+  // Avatar
   Widget _buildAvatarSection() {
     return Column(
       children: [
-        const Text(
+        Text(
           "Avatar",
-          style: TextStyle(
+          style: GoogleFonts.inter(
             fontSize: 24,
             fontWeight: FontWeight.bold,
-            color: Colors.black,
           ),
         ),
-        const SizedBox(height: 8),
-        Image.asset(
-          _selectedAvatar,
+        SizedBox(
           width: 200,
           height: 200,
-          fit: BoxFit.contain,
+          child: Image.asset(
+            _selectedAvatar,
+            fit: BoxFit.contain,
+          ),
         ),
-        const SizedBox(height: 12),
         Text(
           _selectedAvatarName,
-          style: const TextStyle(
+          style: GoogleFonts.inter(
             fontSize: 20,
             fontWeight: FontWeight.bold,
-            color: Colors.black,
           ),
         ),
-      ],
-    );
-  }
-
-  Widget _buildCategoryButtons() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        for (final category in categories)
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: _buildCategoryButton(
-                category.name, category.color, category.label),
-          ),
-      ],
-    );
-  }
-
-  Widget _buildCategoryButton(String category, Color color, String label) {
-    return OutlinedButton(
-      onPressed: () {
-        setState(() {
-          _selectedButton = category;
-        });
-      },
-      style: OutlinedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 12,
-        ),
-        side: const BorderSide(color: Colors.black),
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-      ),
-      child: Row(
-        children: [
-          Container(
-            width: 12,
-            height: 12,
-            color: color,
-          ),
-          const SizedBox(width: 8),
-          Text(
-            label,
-            style: const TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildSelectedCategoryLabel() {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 20.0),
-            child: const Text(
-              "My Avatars",
-              style: TextStyle(
-                fontSize: 18,
+        SizedBox(height: 20),
+        SizedBox(
+          width: 160,
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              backgroundColor: Color(0xFFFECE2E),
+            ),
+            child: Text(
+              'Gunakan',
+              style: GoogleFonts.inter(
+                fontSize: 16,
                 fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 15.0,
-              bottom: 10.0,
-            ),
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 6.0,
-                horizontal: 20.0,
-              ),
-              decoration: BoxDecoration(
-                color: categories
-                    .firstWhere((c) => c.name == _selectedButton)
-                    .color,
-                borderRadius: BorderRadius.circular(60.0),
-              ),
-              child: Text(
-                _selectedButton.toUpperCase(),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
+                color: Colors.white,
               ),
             ),
           ),
         ),
       ],
+    );
+  }
+
+  // Category Button
+  Widget _buildCategoryButton(
+    String text,
+    String role,
+    Color backgroundColor,
+    Color textColor,
+  ) {
+    return SizedBox(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            selectedCategory = role;
+          });
+        },
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          backgroundColor:
+              selectedCategory == role ? backgroundColor : Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            side: selectedCategory == role
+                ? BorderSide.none
+                : BorderSide(color: Colors.black),
+          ),
+        ),
+        child: Text(
+          text,
+          style: GoogleFonts.inter(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+            color: selectedCategory == role ? textColor : Colors.black,
+          ),
+        ),
+      ),
     );
   }
 
@@ -244,12 +237,13 @@ class _InventoryState extends State<Inventory> {
     final category = categories.firstWhere((c) => c.name == _selectedButton);
 
     return GridView.builder(
-      padding: const EdgeInsets.all(8.0),
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
         crossAxisSpacing: 8.0,
-        mainAxisSpacing: 8.0,
-        childAspectRatio: 0.8,
+        mainAxisSpacing: 10.0,
+        childAspectRatio: 1.2,
       ),
       itemCount: category.assets.length,
       itemBuilder: (context, index) {
@@ -263,15 +257,15 @@ class _InventoryState extends State<Inventory> {
           child: Stack(
             clipBehavior: Clip.none,
             children: [
+              // Card
               Card(
-                elevation: 4,
+                elevation: 2,
                 color: category.color,
-                margin: const EdgeInsets.only(top: 50),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
                 child: Padding(
-                  padding: const EdgeInsets.all(10.0),
+                  padding: const EdgeInsets.all(12),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
@@ -282,8 +276,6 @@ class _InventoryState extends State<Inventory> {
                         allowHalfRating: true,
                         itemCount: 5,
                         itemSize: 16,
-                        itemPadding:
-                            const EdgeInsets.symmetric(horizontal: 2.0),
                         itemBuilder: (context, _) => const Icon(
                           Icons.star,
                           color: Colors.amber,
@@ -296,17 +288,19 @@ class _InventoryState extends State<Inventory> {
                   ),
                 ),
               ),
+              // Avatar
               Positioned(
-                left: 2,
-                top: 2,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
+                bottom: 20,
+                left: 0,
+                right: 0,
+                child: Align(
+                  alignment: Alignment.topCenter,
                   child: SizedBox(
-                    width: 120,
-                    height: 160,
+                    width: 100,
+                    height: 100,
                     child: Image.asset(
                       category.assets[index],
-                      fit: BoxFit.contain,
+                      fit: BoxFit.cover,
                     ),
                   ),
                 ),
